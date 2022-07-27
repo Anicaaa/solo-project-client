7754ea30f68deae45ea8fd00ada93b509a63b715
Anica Rahman <anicarahman2009@yahoo.it>
2022-07-27 20:39:05 +0100

    Filter by city done

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,78 @@
 import React from "react";
+import PeopleList from "../db/peopleList";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [data, setData] = useState(PeopleList);
+
+  const filterResult = (people) => {
+    const result = PeopleList.filter((p) => {
+      return p.city === people;
+    });
+    setData(result);
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <main className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="title">Where Next?</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button className="btn" onClick={() => setData(PeopleList)}>
+              <strong>All</strong>
+            </button>
+            <button className="btn" onClick={() => filterResult("Brighton")}>
+              Brighton
+            </button>
+            <button className="btn" onClick={() => filterResult("Cambridge")}>
+              Cambridge
+            </button>
+            <button className="btn" onClick={() => filterResult("Edinburgh")}>
+              Edinburgh
+            </button>
+            <button className="btn" onClick={() => filterResult("Glasgow")}>
+              Glasgow
+            </button>
+            <button className="btn" onClick={() => filterResult("London")}>
+              London
+            </button>
+            <button className="btn" onClick={() => filterResult("Nottingham")}>
+              Nottingham
+            </button>
+            <button className="btn" onClick={() => filterResult("Oxford")}>
+              Oxford
+            </button>
+          </div>
+          <div className="col">
+            <div className="cards">
+              {data.map((values) => {
+                const { id, name, position, image } = values;
+                return (
+                  <>
+                    <div className="card" key={id}>
+                      <div className="card-header">
+                        <img src={image} alt={name} />
+                      </div>
+                      <div className="card-body">
+                        <h2 className="titleProduct">{name}</h2>
+                        <p className="position">
+                          <strong>Position: </strong>
+                          {position}
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
